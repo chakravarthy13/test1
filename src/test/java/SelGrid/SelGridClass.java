4a1af5df5d36f0c8105c4f6f1e6f775eb68d2027
Anjaneyam <Anjaneyam@192.168.0.10>
2020-02-03 08:01:02 +0530

    Commit one Hello World 2

---
+++ src/test/java/SelGrid/SelGridClass.java
@@ -0,0 +1,14 @@
+package SelGrid;
+
+import org.testng.annotations.Test;
+
+public class SelGridClass {
+	
+	@Test
+	public void SelGridMethod() {
+		System.out.println("Hello World!");
+		
+		System.out.println("This is Hello World!! 2");
+	}
+
+}
